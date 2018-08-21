98ad6fb8ac50e847ef04287e43ca42ecbc1bef2c
Rita <contact@ritasolo.com>
2018-08-20 20:04:57 -0400

    setup files and snippets

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+// PSEUDOCODE
+
+// 1. Figure out 10 different cities as potential vacation spots
+// 2. Use those cities in array
+// 3. When user clicks button:
+    // disable default
+    // randomly pick object from array
+        // put animation in to randomly reveal and hide dots on the map
+    // use Travel API to populate the info we want based on the city chosen
+    // display that information in our HTML
+    // give an exit option on the info module that also resets the input
+// 4. STRETCH GOALS
+    // provide buttons linking to Book List and Packing List
+   
+nextTripApp = {};
+
+$(function(){
+    console.log('ready!')
+});
